42bf1a070dce02116e0ccd44e08fb94f7241c9cd
Luan-art <luanlf2003@gmail.com>
2024-07-22 16:02:02 -0300

    first commit

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -13,9 +13,9 @@ export default function TabTwoScreen() {
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
       headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
+        <ThemedText type="title">Olá mundo</ThemedText>
       </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
+      <ThemedText>Testes iniciaisi com React</ThemedText>
       <Collapsible title="File-based routing">
         <ThemedText>
           This app has two screens:{' '}
