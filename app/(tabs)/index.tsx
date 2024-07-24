1ef321a68ce384990213a926d328038273657fd5
Luan-art <luanlf2003@gmail.com>
2024-07-24 10:09:18 -0300

    criação da tela index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,47 +1,70 @@
 import { ThemedText } from '@/components/ThemedText';
-import { View, Text, Image, StyleSheet} from 'react-native';
+import { View, Image, StyleSheet, Touchable, TouchableOpacity } from 'react-native';
 import { Slider } from 'react-native-awesome-slider';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { useSharedValue } from 'react-native-reanimated';
 
-export default function App(){
+export default function App() {
   const progress = useSharedValue(30);
   const min = useSharedValue(6);
   const max = useSharedValue(20);
 
-  return(
-    <View style = { style.container}>
-      <Image
-        source={require("@/assets/images/logo.png")}
-        style={style.logo}
-      />
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={style.container}>
+        <Image
+          source={require("@/assets/images/logo.png")}
+          style={style.logo}
+        />
+        
+        <ThemedText type="title" style={{ color: 'black' }}>20 caracteres</ThemedText>
+        
+        <View style={style.area}>
+          <Slider
+            style={{ height: 50 }}
+            progress={progress}
+            minimumValue={min}
+            maximumValue={max}
+          />
+        </View>
 
-      <ThemedText type="title">20 caracteres</ThemedText>
-      
-      <View style={style.area}>
-       
+        <TouchableOpacity style={style.button}>
+          <ThemedText style={style.buttonText}>Gerar senha</ThemedText>
+        </TouchableOpacity>
       </View>
-    </View>
-  )
+    </GestureHandlerRootView>
+  );
 }
 
 const style = StyleSheet.create({
-  container:{
-    flex:1,
+  container: {
+    flex: 1,
     backgroundColor: "#F3F3FF",
     justifyContent: 'center',
     alignItems: 'center'
   },
-  logo:{
+  logo: {
     marginBottom: 60
   },
-  area:{
+  area: {
     marginTop: 14,
-    marginBottom:14,
+    marginBottom: 14,
     width: '80%',
-    backgroundColor:"#FFF",
+    backgroundColor: "#FFF",
     borderRadius: 8,
     padding: 8
+  },
+  button: {
+    backgroundColor: "#392de9",
+    width: '80%',
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
+    marginBottom: 18
+  },
+  buttonText: {
+    color: "#FFF",
+    fontSize: 20 
   }
-  
-})
- 
+});
